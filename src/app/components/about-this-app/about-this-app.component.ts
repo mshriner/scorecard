907dc319595a3c2bb2c77d80815c36ab099c2ef1
mshriner <marshriner@gmail.com>
2025-07-27 16:46:43 -0500

    feat: update goToAbout method to be async and handle navigation after updates fix: add id to changelog card for smooth scrolling from session storage

--- src/app/components/about-this-app/about-this-app.component.ts
+++ src/app/components/about-this-app/about-this-app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { AppStateService } from '../../services/app-state.service';
@@ -9,8 +9,20 @@ import { AppStateService } from '../../services/app-state.service';
   styleUrls: ['./about-this-app.component.scss'],
   imports: [MatCardModule, MatDividerModule],
 })
-export class AboutThisAppComponent {
+export class AboutThisAppComponent implements OnInit {
   constructor(private readonly appStateService: AppStateService) {
     this.appStateService.setPageTitle(`About This App`);
+  }
+
+  ngOnInit(): void {
+    if (sessionStorage.getItem('goToChangeLog')) {
+      setTimeout(() => {
+        document.getElementById('changelog')?.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        sessionStorage.removeItem('goToChangeLog');
+      }, 1);
+    }
   }
 }
