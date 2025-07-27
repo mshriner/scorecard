4871904831498a17195ceb7c1d252c2e0a8c6015
mshriner <marshriner@gmail.com>
2025-07-27 16:01:10 -0500

    fix test

--- src/app/components/best-round-dialog/best-round-dialog.component.spec.ts
+++ src/app/components/best-round-dialog/best-round-dialog.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { COURSE_EXAMPLE } from '../../models/course';
+import { BestRound, ROUND_EXAMPLE } from '../../models/round';
 import { BestRoundDialogComponent } from './best-round-dialog.component';
 
 describe('BestRoundDialogComponent', () => {
@@ -7,10 +11,23 @@ describe('BestRoundDialogComponent', () => {
   let fixture: ComponentFixture<BestRoundDialogComponent>;
 
   beforeEach(async () => {
+    const bestRound: BestRound = {
+      strokes: ROUND_EXAMPLE.strokes,
+      roundVariety: ROUND_EXAMPLE.roundVariety,
+      course: COURSE_EXAMPLE,
+      bestScoresRecordedDateISO: Array(18).fill(new Date().toISOString()),
+    };
     await TestBed.configureTestingModule({
-      imports: [BestRoundDialogComponent]
-    })
-    .compileComponents();
+      imports: [BestRoundDialogComponent],
+      providers: [
+        provideExperimentalZonelessChangeDetection(),
+        { provide: MatDialogRef, useValue: {} },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: bestRound,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(BestRoundDialogComponent);
     component = fixture.componentInstance;
