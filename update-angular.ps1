8d8143f76db81063d2366b8f8fa9c13f52388a86
mshriner <marshriner@gmail.com>
2026-05-02 19:00:14 -0500

    update angular script no longer needs build-angular

--- update-angular.ps1
+++ update-angular.ps1
@@ -5,7 +5,7 @@ git reset
 git add .\package.json .\package-lock.json
 git commit -m "npm install"
 
-ng update @angular/cli@latest @angular/core@latest @angular/material@latest @angular-devkit/build-angular@latest
+ng update @angular/cli@latest @angular/core@latest @angular/material@latest
 npm install
 
 git add .\package.json .\package-lock.json
