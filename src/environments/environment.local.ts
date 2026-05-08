448c388b47f015ed101a763da0d49ced5708d8c7
mshriner <marshriner@gmail.com>
2026-05-08 04:21:58 -0500

    fix API key try 1

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,10 +1,4 @@
-declare const process: {
-  env: {
-    PUBLIC_GOLF_COURSE_API_KEY: string;
-  };
-};
-
 export const environment = {
   production: false,
-  golfCourseApiKey: process.env.PUBLIC_GOLF_COURSE_API_KEY,
+  golfCourseApiKey: '${PUBLIC_GOLF_COURSE_API_KEY}',
 };
